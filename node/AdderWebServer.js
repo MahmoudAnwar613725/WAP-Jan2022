8c0c4d6fa21c0073346624621544c8620015b91e
MahmoudAnwar613725 <mahmohamed@miu.edu>
2022-01-29 00:17:34 -0600

    commit sample calculator

--- node/AdderWebServer.js
+++ node/AdderWebServer.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require("path");
 const app = express();
 app.listen(8080, ()=>{
-    console.log('Your server is running on port 8080')
+    console.log('Server is running on port 8080')
 })
 
 app.all('/' , (req, res, next)=>{
